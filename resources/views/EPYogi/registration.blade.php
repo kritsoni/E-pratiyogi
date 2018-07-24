82cff06c3fa0579cbac46beebcd2ca6c76f70ad9
Rajs0ni <rajsoni168@gmail.com>
2018-07-24 22:22:37 +0530

    updated

--- resources/views/EPYogi/registration.blade.php
+++ resources/views/EPYogi/registration.blade.php
@@ -3,17 +3,31 @@
 @section('register')
    
    <!-- Outer container -->
-   <div class="container">
+   <div class="container outercontainer">
       <!-- First-Row -->
-       <div class="row">hey</div>
+       <div class="row">E-Pratiyogi</div>
        <!-- Second-Row -->
        <div class="row">
-
-          <div class="row">hey</div>
-          <div class="row">hey</div>
-       
+            <div class="container innercontent">
+                <div class="row">
+                    <button type="button" class="btn button btn-arrow-right">Button1</button>
+                    <button type="button" class="btn button btn-arrow-right">Button2</button>
+                    <button type="button" class="btn button btn-arrow-right">Button3</button>
+                    <button type="button" class="btn button btn-arrow-right">Button4</button>
+                </div>
+                <div class="row">
+                    <div class="container content">
+                        <label>Name:</label><br>
+                        <input type="text"><br>
+                        <label>Password:</label><br>
+                        <input type="passward"><br>
+                        <label>Confirm Password:</label><br>
+                        <input type="passward">
+                    </div>
+                </div>
+            </div>
        </div>  
-   
+       <button type="button" class="btn nextButton">Next Step</button>
    </div>
 
 
