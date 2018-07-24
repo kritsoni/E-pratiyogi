280a5a597d183f9cd5c90a8b5e23cb667c6d2a64
Abhighyaa <abhighyaajain1610@gmail.com>
2018-07-24 14:50:56 +0530

    Initial commits

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -84,8 +84,7 @@
 
                 <div class="links">
                     <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
+                    
                 </div>
             </div>
         </div>
